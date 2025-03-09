902e6faf7aa1d0656eab8507855df93e8917a823
Aangelsepulveda <angelsepulvedasep@gmail.com>
2025-03-09 18:41:16 -0300

    mejora en endpoint

--- src/Modules/Memberships/Submodules/Users/Features/Authenticate/UserAuthenticateEndpoint.cs
+++ src/Modules/Memberships/Submodules/Users/Features/Authenticate/UserAuthenticateEndpoint.cs
@@ -5,7 +5,7 @@ public class UserAuthenticateEndpoint : ICarterModule
     public void AddRoutes(IEndpointRouteBuilder app)
     {
         app.MapPost(
-            "/auth/login-google",
+            "api/auth/login-google",
             async (ISender sender, [FromBody] TokenRequest request) =>
             {
                 string jwtToken = await sender.Send(new UserAuthenticateQuery(request.Token));
